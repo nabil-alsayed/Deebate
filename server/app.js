08cb3c8d34ccbebd5c44b428ef0059c5846619fd
Andre Ibrahim <andreibr@chalmers.se>
2024-09-17 16:03:05 +0200

    implement GET by id & GET all for argument entity
    argumentController now has methods for getting a specific argument or all arguments, no owner verification/authentication implemented yet

--- server/app.js
+++ server/app.js
@@ -6,20 +6,27 @@ var cors = require('cors');
 var history = require('connect-history-api-fallback');
 
 var Argument = require('./models/argument');
-var { createArgument } = require('./controllers/argumentController');
+var {
+  createArgument,
+  getArgumentById,
+  getAllArguments,
+} = require('./controllers/argumentController');
 
 // Variables
 var mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/deebateDB';
 var port = process.env.PORT || 3000;
 
 // Connect to MongoDB
-mongoose.connect(mongoURI).catch(function(err) {
+mongoose
+  .connect(mongoURI)
+  .catch(function (err) {
     console.error(`Failed to connect to MongoDB with URI: ${mongoURI}`);
     console.error(err.stack);
     process.exit(1);
-}).then(function() {
+  })
+  .then(function () {
     console.log(`Connected to MongoDB with URI: ${mongoURI}`); // mistake when forward porting
-});
+  });
 
 // Create Express app
 var app = express();
@@ -32,14 +39,14 @@ app.use(morgan('dev'));
 app.options('*', cors());
 app.use(cors());
 
-// Import routes
-// app.get('/api', function (req, res) {
-// res.json({ message: 'Welcome to your DIT342 backend ExpressJS project!' });
-// });
+// Argument routes
 app.post('/api/arguments', createArgument);
+app.get('/api/arguments', getAllArguments);
+app.get('/api/arguments/:id', getArgumentById);
+
 // Catch all non-error handler for api (i.e., 404 Not Found)
 app.use('/api/*', function (req, res) {
-    res.status(404).json({ 'message': 'Not Found' });
+  res.status(404).json({ message: 'Not Found' });
 });
 
 // Configuration for serving frontend in production mode
@@ -53,25 +60,25 @@ app.use(express.static(client));
 // Error handler (i.e., when exception is thrown) must be registered last
 var env = app.get('env');
 // eslint-disable-next-line no-unused-vars
-app.use(function(err, req, res, next) {
-    console.error(err.stack);
-    var err_res = {
-        'message': err.message,
-        'error': {}
-    };
-    if (env === 'development') {
-        // Return sensitive stack trace only in dev mode
-        err_res['error'] = err.stack;
-    }
-    res.status(err.status || 500);
-    res.json(err_res);
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  var err_res = {
+    message: err.message,
+    error: {},
+  };
+  if (env === 'development') {
+    // Return sensitive stack trace only in dev mode
+    err_res['error'] = err.stack;
+  }
+  res.status(err.status || 500);
+  res.json(err_res);
 });
 
-app.listen(port, function(err) {
-    if (err) throw err;
-    console.log(`Express server listening on port ${port}, in ${env} mode`);
-    console.log(`Backend: http://localhost:${port}/api/`);
-    console.log(`Frontend (production): http://localhost:${port}/`);
+app.listen(port, function (err) {
+  if (err) throw err;
+  console.log(`Express server listening on port ${port}, in ${env} mode`);
+  console.log(`Backend: http://localhost:${port}/api/`);
+  console.log(`Frontend (production): http://localhost:${port}/`);
 });
 
 module.exports = app;
--- server/controllers/argumentController.js
+++ server/controllers/argumentController.js
@@ -1,5 +1,7 @@
 const Argument = require('../models/argument');
+//const Comment = require('../models/comment');
 
+// Create a new argument
 const createArgument = async (req, res) => {
   try {
     const { content, owner } = req.body;
@@ -14,4 +16,35 @@ const createArgument = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
-module.exports = { createArgument };
+
+// Get specific argument by ID
+const getArgumentById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const argument = await Argument.findById(id).populate('comments');
+    if (!argument) {
+      return res.status(404).json({ error: 'Argument not found' });
+    }
+    res.status(200).json(argument);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Get all arguments
+const getAllArguments = async (req, res) => {
+  try {
+    const arguments = await Argument.find().populate('comments');
+    res.status(200).json(arguments);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// TO-DO: Delete specific argument by ID
+
+module.exports = {
+  createArgument,
+  getArgumentById,
+  getAllArguments,
+};
