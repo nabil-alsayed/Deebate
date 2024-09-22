eb26e4cfd9a0aa435a206d78f05935ea5f84a25e
nabil-alsayed <nabilsa@chalmers.se>
2024-09-22 08:29:12 +0200

    Seperate index.js from v1 for Better Modularity

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 // Import the API route module
-const apiRoutes = require('./api.route');
+const v1ApiRoutes = require('./v1/api.route');
 
 // Use the API routes under `/api`
-router.use('/api', apiRoutes);
+router.use('/api', v1ApiRoutes);
 
 module.exports = router;
 
