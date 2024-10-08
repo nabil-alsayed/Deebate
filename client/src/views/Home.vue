9d5d4336b66fd304cf2f52c82473179d626477ce
Yaro <ursul@student.chalmers.se>
2024-10-08 17:28:11 +0200

    Add categories but pressing them doesn't renew the debate list (filtering doesn't work)

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -6,97 +6,76 @@
       </div>
       <div class="col-md-6 main-content">
         <SearchBar @search-results="updateDebates" />
-        <DebateList :searchResults="searchResults" />
+        <DebateList :debates="filteredDebates" />
       </div>
-      <div class="col-md-3 leaderboard">
-        <Leaderboard />
+      <div class="col-md-3 category-selector">
+        <CategorySelector @category-selected="filterDebatesByCategory" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { ref, computed } from 'vue'
 import MenuBar from '@/components/MenuBar.vue'
 import SearchBar from '@/components/SearchBar.vue'
 import DebateList from '@/components/DebateList.vue'
-import Leaderboard from '@/components/Leaderboard.vue'
+import CategorySelector from '@/components/CategorySelector.vue'
+import { Api } from '@/Api'
 
 export default {
   components: {
     MenuBar,
     SearchBar,
     DebateList,
-    Leaderboard
+    CategorySelector
   },
-  data() {
+  setup() {
+    const debates = ref([])
+    const selectedCategory = ref('')
+
+    const filteredDebates = computed(() => {
+      if (selectedCategory.value) {
+        return debates.value.filter(debate => debate.category === selectedCategory.value)
+      }
+      return debates.value
+    })
+
+    async function updateDebates(results) {
+      debates.value = results
+    }
+
+    async function filterDebatesByCategory(category) {
+      selectedCategory.value = category
+      try {
+        const response = await Api.get(`/v1/debates?category=${category}`)
+        debates.value = response.data.debates
+      } catch (error) {
+        console.error('Error fetching debates:', error)
+        debates.value = []
+      }
+    }
+
     return {
-      debates: [],
-      searchQuery: '',
-      searchResults: []
-    }
-  },
-  computed: {
-    filteredDebates() {
-      return this.debates.filter(debate =>
-        debate.topic.toLowerCase().includes(this.searchQuery.toLowerCase())
-      )
-    }
-  },
-  methods: {
-    handleSearch(query) {
-      this.searchQuery = query
-    },
-    updateDebates(results) {
-      this.searchResults = results
+      filteredDebates,
+      updateDebates,
+      filterDebatesByCategory
     }
   }
 }
 </script>
 
-<style>
+<style scoped>
 .container-fluid {
   background-color: #f0f0f0;
 }
 
-.menu-bar {
-  padding: 10px;
-}
-
-.main-content {
+.menu-bar, .main-content, .category-selector {
   padding: 20px;
-  overflow-y: auto;
-}
-
-.leaderboard {
-  padding: 20px;
-}
-
-/* Custom styles */
-.custom-header {
-  font-size: 24px;
-  color: #333;
-}
-
-.custom-paragraph {
-  line-height: 1.6;
-  color: #666;
-}
-
-#search-container {
-  margin-bottom: 20px;
-}
-
-#debate-list {
-  border: 1px solid #ddd;
-  border-radius: 5px;
-}
-
-.highlight {
-  background-color: yellow;
 }
 
 @media (max-width: 767px) {
-  .menu-bar, .leaderboard {
+  .menu-bar, .category-selector {
     position: static;
     width: 100%;
     height: auto;
